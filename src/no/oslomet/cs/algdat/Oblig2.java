059482554e39c504512371397391808295793a5e
Farhan <s341869@oslomet.no>
2020-10-05 18:56:27 +0200

    Oppgave 3-b : Subliste metoden her skal returnere en liste som inneholder verdier fra intervallet fra:til. Vi skal bruke også kontrollmetode,det er lov til å bli intervall tomt her.

--- src/no/oslomet/cs/algdat/Oblig2.java
+++ src/no/oslomet/cs/algdat/Oblig2.java
@@ -29,39 +29,40 @@ class DobbeltLenketListe<T> implements Liste<T> {
     private Node<T> hale;          // peker til den siste i listen
     private int antall;            // antall noder i listen
     private int endringer;   // antall endringer i listen
+
     //Oppgave 3-a
     //Hjelpemetode
-    private Node<T> finnNode(int indeks){
+    private Node<T> finnNode(int indeks) {
         Node<T> p;     //p: returnNode her
-        if(indeks< antall/2){
-            p=hode;
-            for(int i=1; i<indeks;i++){
-                p=p.neste;
+        if (indeks < antall / 2) {
+            p = hode;
+            for (int i = 1; i < indeks; i++) {
+                p = p.neste;
             }
-        }
-        else{
-            p=hale;
-            for(int i=1; i<(antall-indeks);i++){
-                p=p.forrige;
+        } else {
+            p = hale;
+            for (int i = 1; i < (antall - indeks); i++) {
+                p = p.forrige;
             }
         }
         return p;
     }
 
     // legge til en konstruktør
-    public DobbeltLenketListe(){
+    public DobbeltLenketListe() {
 
         hode = hale = null;
         antall = 0;
         endringer = 0;
     }
+
     // Oppgave 1
     // konstruktør
     public DobbeltLenketListe(T[] a) {
         this();
 
         int i = 0;
-        for (; i < a.length && a[i]==null; i++) ; // fikset her
+        for (; i < a.length && a[i] == null; i++) ; // fikset her
 
         if (i < a.length) {
             Node<T> p = hode = hale = new Node<>(a[i], null, null);  // den første noden
@@ -79,6 +80,24 @@ class DobbeltLenketListe<T> implements Liste<T> {
 
 
     }
+
+    // Oppgave 3-b
+    public Liste<T> subliste(int fra, int til) {
+        fratilKontroll(antall, fra, til);
+        Node<T> p = finnNode(fra);
+        Liste<T> h = new DobbeltLenketListe<T>();
+        return h;
+    }
+
+    private void fratilKontroll(int antall, int fra, int til) {
+        if(fra<0)
+            throw new IndexOutOfBoundsException();
+        if(til>antall)
+            throw new IndexOutOfBoundsException();
+        if (fra> til)
+            throw new IllegalArgumentException();
+    }
+
     // Oppgave 1
     @Override
     public int antall() {
